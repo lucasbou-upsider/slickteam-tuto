0d6a3c5475af57d3be60ed55cbbcbb4f0ba7863d
Slickteam Ordi <ordi@slickteam.fr>
2025-03-05 14:21:26 +0100

    J'ajoute les fichiers

---
+++ scripts/main.js
@@ -0,0 +1,53 @@
+//test
+
+let nom = document.getElementById("input")
+console.log(nom)
+let phraseoumots = ""
+
+let button = document.getElementById("btnValiderMot")
+clique_btn_verifictation()
+
+function clique_btn_verifictation(){
+    button.addEventListener("click", function(){
+        console.log("bonton appuyé")
+        verifictation_mot()
+    });
+}
+
+document.addEventListener('keydown', (Event) => {
+});
+
+
+function verifictation_mot(){
+    let mot_ecrit = nom.value
+    console.log(mot_ecrit)
+    if(phraseoumots === "mots"){
+        if(mot_ecrit === listeMots[0]){
+            score++
+            console.log("c le bon mot")
+        }
+        
+    }
+    if(phraseoumots === "phrases"){
+        if(mot_ecrit === listePhrases[0]){
+            score++
+            console.log("c la bonne phrase")
+        }
+    }
+}
+
+
+
+//bouton phrase et bouton mot
+let btnphrase = document.getElementById("phrases")
+let btnmots = document.getElementById("mots")
+
+
+btnmots.addEventListener("click", function(){
+    phraseoumots = "mots"
+    console.log(phraseoumots)
+    })
+btnphrase.addEventListener("click", function(){
+    phraseoumots = "phrases"
+    console.log(phraseoumots)
+    })
